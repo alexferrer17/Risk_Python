b402b4065c5aac809795da00e671a6350f24e834
Alejandro Ferrer-Peasley <36723243+alexferrer17@users.noreply.github.com>
2023-02-20 07:55:42 -0800

    Fixed the bug where the sum troops != 30
    For both players

--- risk_main.py
+++ risk_main.py
@@ -19,29 +19,40 @@ territories = [
 # Total number of troops
 total_troops = 90
 
+# Total number of player
+num_players = 3
+
 # Randomly assign territories to players
 random.shuffle(territories)
 player1_territories = territories[:14]
 player2_territories = territories[14:28]
 neutral_territories = territories[28:]
 
-# Distribute troops randomly among players
+# Distribute troops for player 1
+player1_troops = [random.randint(1, 3) for _ in range(14)]
+
+#Ensure that the total troops for player 1 adds to 30
 player1_troops = [random.randint(1, total_troops//30) for _ in range(14)]
+while sum(player1_troops) != 30:
+    index = random.randint(0, 13)
+    if sum(player1_troops) < 30:
+        player1_troops[index] += 1
+    else:
+        player1_troops[index] -= 1
+
+# Distribute troops for player 1
+player2_troops = [random.randint(1, 3) for _ in range(14)]
+
+#Ensure that the total troops for player 2 adds to 30
 player2_troops = [random.randint(1, total_troops//30) for _ in range(14)]
-neutral_troops = [random.randint(1, total_troops//30) for _ in range(14)]
+while sum(player2_troops) !=30:
+    index = random.randint(0, 13)
+    if sum(player2_troops) < 30:
+        player2_troops[index] += 1
+    else:
+        player2_troops[index] -= 1
 
-# Make sure that each player has at most 30 troops
-if sum(player1_troops) > 30:
-    excess_troops = sum(player1_troops) - 30
-    player1_troops[-1] -= excess_troops
-
-if sum(player2_troops) > 30:
-    excess_troops = sum(player2_troops) - 30
-    player2_troops[-1] -= excess_troops
-
-if sum(neutral_troops) > 30:
-    excess_troops = sum(neutral_troops) - 30
-    neutral_troops[-1] -= excess_troops
+neutral_troops = [random.randint(1, 3) for _ in range(14)]
 
 # Dictionary to store information about players
 players = {
