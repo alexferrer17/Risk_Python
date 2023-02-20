6f610ae08c790efe1a476584cde859d423ffae58
Alejandro Ferrer-Peasley <36723243+alexferrer17@users.noreply.github.com>
2023-02-20 10:10:44 -0800

    separated player and main class
    Clean so its easier to read and to extend the code

---
+++ player.py
@@ -0,0 +1,4 @@
+class Player:
+    def __init__(self, territories, troops):
+        self.territories = territories
+        self.troops = troops
--- risk_main.py
+++ risk_main.py
@@ -1,4 +1,5 @@
 import random
+from player import Player
 
 # List of all territories in the game
 territories = [
@@ -40,7 +41,7 @@ while sum(player1_troops) != 30:
     else:
         player1_troops[index] -= 1
 
-# Distribute troops for player 1
+# Distribute troops for player 2
 player2_troops = [random.randint(1, 3) for _ in range(14)]
 
 #Ensure that the total troops for player 2 adds to 30
@@ -54,28 +55,21 @@ while sum(player2_troops) !=30:
 
 neutral_troops = [random.randint(1, 3) for _ in range(14)]
 
+# Create Player objects for each player
+player1 = Player(player1_territories, player1_troops)
+player2 = Player(player2_territories, player2_troops)
+neutral = Player(neutral_territories, neutral_troops)
+
 # Dictionary to store information about players
 players = {
-    "player1": {
-        "territories": player1_territories,
-        "troops": player1_troops
-    },
-    "player2": {
-        "territories": player2_territories,
-        "troops": player2_troops
-    },
-    "neutral": {
-        "territories": neutral_territories,
-        "troops": neutral_troops
-    }
+    "player1": player1,
+    "player2": player2,
+    "neutral": neutral
 }
 
 # Print information about players
-for player, info in players.items():
+for player, obj in players.items():
     print(f"Player: {player}")
-    print(f"Territories: {info['territories']}")
-    print(f"Troops: {info['troops']}")
-    print("\n")
-
-#Day 1 setup board with all the territories
-#Day 2 setup the attacking, defending, and moving troops
+    print(f"Territories: {', '.join(obj.territories)}")
+    print(f"Total troops: {sum(obj.troops)}")
+    print()
